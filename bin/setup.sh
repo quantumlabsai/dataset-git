03ca5390449e8c94763df2726e4ce8492c575deb
Felipe Gerard <felipe.gerard.c@gmail.com>
2018-10-28 20:06:54 -0600

    setup.sh mejorado

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,4 +2,8 @@
 
 git remote rm origin
 cp -v bin/pre-commit .git/hooks/
-cp -v bin/post-commit .git/hooks
+cp -v bin/post-commit .git/hooks/
+mkdir -p s3-sync-origin
+mkdir -p dataset/train
+mkdir -p dataset/val
+cp -v bin/sync.sh s3-sync-origin/
